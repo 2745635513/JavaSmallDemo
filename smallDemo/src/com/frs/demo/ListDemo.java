a182c68d409f679c02ee0e3d02d1800c1513a8ad
27435513@qq.com <27435513@qq.com>
2019-10-22 16:29:59 +0800

    面试题测试

--- smallDemo/src/com/frs/demo/ListDemo.java
+++ smallDemo/src/com/frs/demo/ListDemo.java
@@ -1,25 +1,37 @@
 package com.frs.demo;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class ListDemo {
     public static void main(String[] args) {
-        Integer[] age={12,3,18,23};
-        List list = Arrays.asList(age);//不能进行增删操作
-        List<Integer> list1= Stream.of(age).collect(Collectors.toList());
+//        Integer[] age={12,3,18,23};
+//        List list = Arrays.asList(age);//不能进行增删操作
+//        List<Integer> list1= Stream.of(age).collect(Collectors.toList());
+//
+//        String[] arrays = {"a", "b", "c"};
+//        List<String> listStrings = Stream.of(arrays).collect(Collectors.toList());
+//
+//
+//        Integer[] ageCopy=list1.stream().toArray(Integer[]::new);//
+//
+//        System.out.println(0.1+0.2>0.3);
+//        System.out.println(0.1F+0.2F);
 
-        String[] arrays = {"a", "b", "c"};
-        List<String> listStrings = Stream.of(arrays).collect(Collectors.toList());
+        List list=new ArrayList();
+        list.add(1);
+        list.add(2);
+        list.add(1,3);
+        List list1=new LinkedList(list);
+        list.addAll(list1);
 
-
-        Integer[] ageCopy=list1.stream().toArray(Integer[]::new);//
-
-        System.out.println(0.1+0.2>0.3);
-        System.out.println(0.1F+0.2F);
-
+        list=list.subList(2,5);
+        list.clear();
+        Iterator iterator=list.iterator();
+        while (iterator.hasNext()){
+            System.out.print(iterator.next() +" ");
+        }
 
     }
 }
--- smallDemo/src/com/frs/demo/StringDemo.java
+++ smallDemo/src/com/frs/demo/StringDemo.java
@@ -2,8 +2,8 @@ package com.frs.demo;
 
 public class StringDemo {
     public static void main(String[] args) {
-        String nana=new String(" nan da ") ;
-        String mama=nana.trim();//截取两边的空格
+//        String nana=new String(" nan da ") ;
+//        String mama=nana.trim();//截取两边的空格
 //        System.out.println(nana==mama);//  false
 //        System.out.println(nana.equals(mama)); // true
 //        nana.split("");
@@ -14,7 +14,10 @@ public class StringDemo {
 //            sb.append(list.get(i));
 //        }
 //        System.out.println(sb.toString());
-        System.out.println(mama.length()+","+nana.length());
+//        System.out.println(mama.length()+","+nana.length());
 
+        //中文的长度
+        String str="Java 你好";
+        System.out.println(str.length());
     }
 }
